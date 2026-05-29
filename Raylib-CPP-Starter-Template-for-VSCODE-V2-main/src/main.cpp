b211d4410c7e58b42627dc47682f6b01cb5eaa07
Diego Ruiz <diegoruizgonzalez743@gmail.com>
2026-05-29 12:13:56 -0600

    Mejora Main

--- Raylib-CPP-Starter-Template-for-VSCODE-V2-main/src/main.cpp
+++ Raylib-CPP-Starter-Template-for-VSCODE-V2-main/src/main.cpp
@@ -1,17 +1,20 @@
 #include <raylib.h>
+#include "game.h"
 
 int main(){
     Color gris{29, 29, 27, 255};
-    int alturaVent = 800;
-    int anchoVent = 600;
 
-    InitWindow(alturaVent, anchoVent, "Space Invaders");
-    SetTargetFPS(60);
+    InitWindow(ANCHO_VENT, ALTO_VENT, "Space Invaders");
+    SetTargetFPS(FPS);
     while(WindowShouldClose() == false){
 
         BeginDrawing();
         ClearBackground(gris);
+        DrawText("SPACE INVADERS", 100, 250, 40, WHITE);
+
         EndDrawing();
     }
     CloseWindow();
+    
+    return 0;
 }
